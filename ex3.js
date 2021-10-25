49aa1bf7d11e003b79937f1b77b6a2c509b3012c
johnnyrps <johnnyrps@gmail.com>
2021-10-25 20:51:37 -0300

    Aula04-update

--- ex3.js
+++ ex3.js
@@ -2,9 +2,21 @@
 let primeiroNumero = 10;
 let segundoNumero = 2;
 let terceiroNumero = 5;
-let olá = "olá"
+let ola = "olá"
 
-console.log(primeiroNumero+segundoNumero+terceiroNumero+segundoNumero+1+olá+23);
+console.log(primeiroNumero+segundoNumero+terceiroNumero+segundoNumero+1+ola+23);
 console.log(primeiroNumero-segundoNumero);
 console.log(primeiroNumero*segundoNumero);
 console.log(primeiroNumero/segundoNumero);
+
+let x = 10;
+let y = 5;
+
+console.log(x + y);
+console.log(x * y);
+
+let z = "1" + 1;
+
+z = "olá" + 23;
+
+console.log(y+"-"+z);
--- operacoes.js
+++ operacoes.js
@@ -23,4 +23,14 @@ console.log(10 > 5);
 //comparação de menor
 console.log(10 < 5);
 
+//Atribuição
+var x = 7;
+const y = 6;
+let z = 2;
+
+let soma = x + y + z;
+let subtracao = x - y;
+console.log(soma);
+console.log(subtracao);
+
 //comparação 
