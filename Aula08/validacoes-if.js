308c32d1987fae298d0803db47ca661a83550dd8
johnnyrps <johnnyrps@gmail.com>
2021-11-02 23:40:52 -0300

    aula08

---
+++ Aula08/validacoes-if.js
@@ -0,0 +1,24 @@
+// let dia = "quarta";
+
+// if(dia == "domingo"){
+//     console.log("Vou para a praia!");
+// }else if(dia == "sexta"){
+//     console.log("Vou para o cinema!");
+// }else if(dia == "segunda"){
+//     console.log("Vou tomar um café!");
+// }
+// else{
+//     console.log("Fico em casa!");
+// }
+
+//Código condição idade acesso
+let idade = 14;
+let acesso = '';
+
+if(idade < 16){
+    console.log(acesso = 'Acesso proibido!');
+}else if(idade >=16 && idade <=18){
+    console.log(acesso = 'Acesso liberado somente com acompanhante maior de idade!');
+}else{
+    console.log(acesso = 'Acesso liberado!');
+}
