5ebbd03ebeb7564bb94d063374320b7a3f37caae
johnnyrps <johnnyrps@gmail.com>
2021-11-02 21:59:03 -0300

    atualizacao

---
+++ Node/aula04.js
@@ -0,0 +1,17 @@
+//Atribuição
+/* var x = 7;
+const y = 6;
+let z = 2;
+
+let soma = x + y + z; //soma
+let subtracao = x - y; //subtração
+
+console.log(soma);
+console.log(subtracao);
+*/
+
+
+let nome = "Johnny";
+let idade = 36;
+
+console.log(nome+"-"+idade);
--- Node/nutricionistaEx4.js
+++ Node/nutricionistaEx4.js
@@ -1,4 +1,19 @@
-//Programa para saber IMC (Indice de Massa Corporal) dos pacientes
-let Nome = {
-    
-}
+//Programa para saber IMC (Indice de Massa Corporal) de um dos pacientes conforme tabela recebida:
+let nome = "Marcos Santana";
+let idade = 25;
+let peso = 79;
+let altura = 1.80;
+let plano = "ouro";
+let imc = peso/ ( altura*altura );
+
+console.log(nome+ " tem plano "+plano+" e" +" tem "+idade+" anos e seu índice de massa corporal é de "+imc);
+
+nome = "Ana";
+sobrenome = "Paula";
+idade = 26;
+peso = 55.0;
+altura = 1.62;
+plano = "Prata";
+imc = peso/ (altura*altura);
+
+console.log(nome + "    " + sobrenome + "   tem    " + idade + " anos "+"   peso   " + peso + "  altura   " + altura + "   plano   " + plano+ "  e sua imc é    " +imc);
