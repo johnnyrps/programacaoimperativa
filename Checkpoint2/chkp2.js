c52d2ec14ae79d14bbfa0ba3b00b89c8b8405139
johnnyrps <johnnyrps@gmail.com>
2021-11-28 17:24:19 -0300

    checkp2-prog-imp

---
+++ Checkpoint2/chkp2.js
@@ -0,0 +1,78 @@
+// function soletrar(texto) {
+//     console.log(texto.replace('-', '').
+//     toLocaleUpperCase().split("").join
+//     ("-"));
+
+// }
+
+// soletrar("digital-house");
+// soletrar("ctd");
+
+// let numeros=[10, 8, 7, 99, 0, 1, 6]
+// numeros.sort(function(a,b){return b-a;});
+
+// console.log(numeros);
+
+// let idade = 1
+// if (idade >= 16){
+//     console.log("ok você tem mais de 16 ou é maior de idade!");
+// }
+    
+// let $total = 10
+//    if ($total < 10)
+//     $total_gerente = 10;
+//     console.log("teste")
+
+//     let nomePessoa = "Johnny"
+
+//     for (var i=0; i<50; i+=10){
+//         console.log(i);
+//     }
+//     console.log(i);
+
+//     function soma(a=1, b=3)
+//     {return a+b;}
+
+//     console.log(soma());
+//     console.log(soma(3));
+//     console.log(soma(1,2));
+
+    // let x=5;
+    // let y = x++ + ++x;
+    // console.log("y= "+y);
+    // x=3;
+    // y=x*(x + 1)*x++;
+    // x=5;
+    // y=3;
+    // y*= x+1
+    // console.log("x= "+x);
+    // console.log("y= "+y);
+
+    // // let frutasAmarelas=['Melão', 'Mamão', 'Limão siciliano'];
+    // let frutasVermelhas=['Morango', 'Cerejas', 'Maçã red'];
+    // let frutasVerdes=['Limão', 'Kiwi', 'Maçã verde'];
+
+    // frutasVermelhas1=frutasVermelhas.map(fruta => fruta.toUpperCase());
+    // let todasFrutas=[...frutasAmarelas,...frutasVermelhas1,...frutasVerdes];
+
+    // console.log(todasFrutas);
+
+    // let valor = 10;
+    // let fatorial = 1;
+    // for (let i = valor; i > 1; i--){
+    //     fatorial*=i;}
+    // console.log("O número "+valor+" Tem valor fatorial = "+fatorial);
+
+    // let frutasAmarelas=['Melão', 'Mamão', 'Limão'];
+    // let [fruta1, fruta2, fruta3] = frutasAmarelas;
+
+    // console.log(fruta1);
+    // console.log(fruta2);
+    // console.log(fruta3);
+    // console.log(frutasAmarelas);
+
+    var z=0;
+    for (var i=20; i<50; i+=10){
+        z+=i;
+    }
+    console.log(z);
