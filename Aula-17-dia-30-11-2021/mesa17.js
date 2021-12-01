6fd8ee4b9637c629bbf9a2e234f51a64671cffa0
johnnyrps <johnnyrps@gmail.com>
2021-12-01 20:49:22 -0300

    mesa17

--- Aula-17-dia-30-11-2021/mesa17.js
+++ Aula-17-dia-30-11-2021/mesa17.js
@@ -1,6 +1,25 @@
-let idades = [22, 25, 14, 19, 15];
+// let idades = [22, 25, 14, 19, 15];
 
-let pares = idades.map(function(item){
-    return item %2 ===0;
-});
-console.log(pares);
+// let pares = idades.map(function(item){
+//     return item %2 ===0;
+// });
+// console.log(pares);
+
+let Numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+let Soma = Numeros.reduce((acumulador, valor) => {return acumulador + valor});
+
+console.log(Soma);
+
+let NovoArray = Numeros.map((valor, index) => {return index / Soma});
+
+console.log(NovoArray);
+console.log(NovoArray);
+// Crie uma função que receba um array de palavras e um número, e que retorna uma matriz (array) com apenas as palavras que têm mais 
+//do que uma quantidade de letras maior do que o número informado. (verifique como funciona o método filter ())
+
+let Palavras = ['Carro', 'Boneca', 'Televisão', 'Notebook'];
+
+let PalavrasMaior = Palavras.filter((palavras) => {return palavras.length > 5})
+
+console.log(PalavrasMaior);
