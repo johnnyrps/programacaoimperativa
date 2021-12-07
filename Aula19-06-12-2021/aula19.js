c8c1f48797e73c5c56694053e5ca6c22508928e0
johnnyrps <johnnyrps@gmail.com>
2021-12-06 21:19:52 -0300

    mesa19-06-12-2021

---
+++ Aula19-06-12-2021/aula19.js
@@ -0,0 +1,64 @@
+// Tem-se um conjunto de dados contendo a altura e o sexo (M ou F) de 15 pessoas.
+//  Faça um programa que calcule e escreva:
+
+// a maior e a menor altura do grupo;
+// a média de altura das mulheres;
+// o número de homens.
+
+const pessoas = require("./pessoas");
+let listaUsuarios = [];
+
+function Pessoas(sexo, nome, altura) {
+  this.sexo = sexo;
+  this.nome = nome;
+  this.altura = altura;
+}
+
+for (let i = 0; i < pessoas.length; i++) {
+  listaUsuarios.push(new Pessoas(pessoas[i].sexo, pessoas[i].nome, pessoas[i].altura));
+}
+
+// console.log(listaUsuarios);
+
+// a maior e a menor altura do grupo;
+
+let alturas = []
+for (let i = 0; i < pessoas.length; i++){
+    alturas.push(listaUsuarios[i].altura)
+}
+  
+// console.log(Math.max(...alturas));
+// console.log(Math.min(...alturas));
+
+
+// a média de altura das mulheres;
+
+function mediaAlturaMulheres(listaUsuarios) {
+    let listaMulheres = listaUsuarios
+      .filter((item) => {
+        return item.sexo === "F";
+      })
+      .map((item) => {
+        return item.altura;
+      });
+  
+    return (
+      listaMulheres.reduce((acc, num) => {
+        return acc + num;
+      }) / listaMulheres.length
+    );
+  };
+//   console.log(mediaAlturaMulheres(listaUsuarios));
+  
+
+
+// o número de homens.
+
+function numeroHomens(listaUsuarios) {
+    return listaUsuarios
+      .filter((item) => {
+        return item.sexo === "M";
+      }).length;
+  }
+  console.log(numeroHomens(listaUsuarios));
+
